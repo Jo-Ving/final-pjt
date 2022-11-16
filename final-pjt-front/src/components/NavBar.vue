4bc50e25d6a2f17d6b9b78cd94508571d631a855
userJu <boochickenood@gmail.com>
2022-11-16 16:34:59 +0900

    Feat: Navbar Component

--- final-pjt-front/src/components/NavBar.vue
+++ final-pjt-front/src/components/NavBar.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>Navbar</div>
+  <div>
+    <div>Logo</div>
+    <div>
+      <ul>
+        <li>HOME</li>
+        <li>내가 찜한 콘텐츠</li>
+      </ul>
+      <ul>
+        <li>검색</li>
+        <li>햄버거바</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "NavBar",
+};
 </script>
 
 <style></style>

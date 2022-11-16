b5a43a341fe63ac9b0805dc3c3aed014ca09eea6
userJu <boochickenood@gmail.com>
2022-11-16 16:31:52 +0900

    Feat: 라우터 기능을 정의한 browserRouter 생성

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <BrowserRouter />
   </div>
 </template>
+<script>
+import BrowserRouter from "./BrowserRouter.vue";
+export default {
+  name: "app",
+  components: { BrowserRouter },
+};
+</script>
 
 <style>
 #app {
---
+++ final-pjt-front/src/BrowserRouter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">HOME</router-link> |
+      <router-link to="/auth">Login</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BrowserRouter",
+};
+</script>
+
+<style></style>

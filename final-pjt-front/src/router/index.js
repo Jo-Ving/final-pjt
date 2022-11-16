c5c42f0b50df88e1fe85baac698c04cd427ad9b8
userJu <boochickenood@gmail.com>
2022-11-16 16:11:17 +0900

    Feat: make basic views
    AuthPage
    MainPage

--- final-pjt-front/src/router/index.js
+++ final-pjt-front/src/router/index.js
@@ -1,24 +1,30 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import MainPage from "../views/MainPage.vue";
+import AuthPage from "../views/AuthPage.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "main",
+    component: MainPage,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/auth",
+    name: "auth",
+    component: AuthPage,
   },
+  // {
+  //   path: "/auth",
+  //   name: "auth",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  // },
 ];
 
 const router = new VueRouter({

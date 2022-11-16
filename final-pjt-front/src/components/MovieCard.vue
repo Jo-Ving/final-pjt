ee1199e9cc2f6eddaecf41d3fa294448aedff350
userJu <boochickenood@gmail.com>
2022-11-16 16:04:25 +0900

    Feat: basic component 생성
    Button
    Footer
    Input
    Main
    Movie
    Navbar

---
+++ final-pjt-front/src/components/MovieCard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>MovieCard</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ final-pjt-front/src/components/mainContainer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>MainContainer</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>

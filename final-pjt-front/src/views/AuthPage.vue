d7094f2744181aedbc02a2063d1a3e5d19821955
userJu <boochickenood@gmail.com>
2022-11-17 11:01:34 +0900

    Feat: login Input Component 연결
    login을 위해 InputComponent를 생성하고
    label과 input 기능을 분리하였다

--- final-pjt-front/src/views/AuthPage.vue
+++ final-pjt-front/src/views/AuthPage.vue
@@ -1,9 +1,56 @@
 <template>
-  <div>Auth</div>
+  <div>
+    <h1>Login</h1>
+    <form action="submit" @click.prevent>
+      <div>
+        <InputComponent
+          :userInput="email"
+          :labelName="`email*`"
+          @inputFromChild="getEmailValue"
+        />
+      </div>
+      <div>
+        <InputComponent
+          :userInput="password"
+          :labelName="`password*`"
+          @inputFromChild="getPasswordValue"
+        />
+      </div>
+      <button @click="onLotinButtonClick">Login</button>
+    </form>
+    <p>
+      Don't have an account?
+      <button>Sign up here</button>
+    </p>
+  </div>
 </template>
 
 <script>
-export default {};
+import InputComponent from "../components/InputComponent.vue";
+
+export default {
+  name: "MainPage",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  components: {
+    InputComponent,
+  },
+  methods: {
+    getEmailValue(email) {
+      console.log(email, "🍕");
+    },
+    getPasswordValue(password) {
+      console.log(password, "🥖");
+    },
+    onLotinButtonClick(e) {
+      console.log(e.target);
+    },
+  },
+};
 </script>
 
 <style></style>
--- final-pjt-front/src/views/MainPage.vue
+++ final-pjt-front/src/views/MainPage.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="container">
-    <NavBar />
     <div>MainPage</div>
     <FooterComponent />
   </div>
 </template>
 
 <script>
-import NavBar from "../components/NavBar.vue";
 import FooterComponent from "../components/FooterComponent.vue";
 export default {
   name: "MainPage",
   components: {
-    NavBar,
     FooterComponent,
   },
 };
@@ -21,6 +18,5 @@ export default {
 <style scoped>
 .container {
   /* background-color: #111827; */
-  background-color: var(--gray8);
 }
 </style>

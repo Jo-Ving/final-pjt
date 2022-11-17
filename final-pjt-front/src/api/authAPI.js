c821e67cc2c8b179445b5c4665aa9e61e83e0b1d
userJu <boochickenood@gmail.com>
2022-11-17 12:39:12 +0900

    Feat: login을 위한 fetchLogin axios 제작
    instance로 create 확인
    함수로 호출 후 통신

--- final-pjt-front/src/api/authAPI.js
+++ final-pjt-front/src/api/authAPI.js
@@ -1,3 +1,16 @@
 import axios from "axios";
 
-console.log(axios);
+const instance = axios.create({
+  baseURL: "",
+  headers: {},
+  timeout: 50000,
+});
+
+export const fetchLogin = async ({ email, password }) => {
+  try {
+    const data = await instance.post("/login", { email, password });
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};

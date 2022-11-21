285576cf2294fcbcd3cb026af1c20fff3a521394
userJu <boochickenood@gmail.com>
2022-11-21 14:47:22 +0900

    Feat: review backend 기능 개발
    review create 기능 개발
    user, movie id 에러 해결

--- final-pjt-front/src/api/endpoints.js
+++ final-pjt-front/src/api/endpoints.js
@@ -3,4 +3,6 @@ export const backendBaseUrl = `http://127.0.0.1:8000`;
 export const apiEndpoint = {
   signUp: `/accounts/signup/`,
   login: `/accounts/token/`,
+  movieDetail: `/movies/19995/`,
+  movieReview: `/movies/19995/reviews/`,
 };
